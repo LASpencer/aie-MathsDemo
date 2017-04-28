3d3caa6a2f2574304b686eb7efce1664ce0c62d9
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-04-28 18:32:55 +0930

    AABox collision with point

--- MathsDemo/project2D/AABox.cpp
+++ MathsDemo/project2D/AABox.cpp
@@ -10,3 +10,19 @@ AABox::AABox()
 AABox::~AABox()
 {
 }
+
+bool AABox::doesCollide(Collider * other)
+{
+	// Calls method on other collider to test collision with AABox
+	return other->doesCollideWithAABox(this);
+}
+
+bool AABox::doesCollide(Vector2 point)
+{
+	if (point[0]<m_min[0] || point[0]>m_max[0] || point[1]<m_min[1] || point[1]>m_max[1]) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
--- MathsDemo/project2D/AABox.h
+++ MathsDemo/project2D/AABox.h
@@ -5,9 +5,22 @@ class AABox :
 {
 public:
 	AABox();
+	AABox(Vector2 min, Vector2 max);
 	~AABox();
 
-protected:
+	// test collision with Collider of unknown concrete type
+	virtual bool doesCollide(Collider* other);
 
+	// test collision with point
+	virtual bool doesCollide(Vector2 point);
+
+	// test collision with each collider subclass
+	virtual bool doesCollideWithAABox(AABox* box) = 0;
+	virtual bool doesCollideWithOBox(OBox* box) = 0;
+	virtual bool doesCollideWithCircle(CircleCollider* circle) = 0;
+
+protected:
+	Vector2 m_min;
+	Vector2 m_max;
 };
 
