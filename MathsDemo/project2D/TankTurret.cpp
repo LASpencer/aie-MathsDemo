c34ddde79e41bc39d993250d59efe7bbdceed168
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-05-12 21:42:36 +0930

    Moved testing to new file

--- MathsDemo/project2D/TankTurret.cpp
+++ MathsDemo/project2D/TankTurret.cpp
@@ -2,7 +2,6 @@
 #include "Texture.h"
 #include "Input.h"
 #include "OBox.h"
-
  
 TankTurret::TankTurret() : m_turretSprite(new aie::Texture("./textures/barrelGreen.png"))
 {
@@ -21,7 +20,7 @@ void TankTurret::update(float deltaTime)
 
 void TankTurret::draw(aie::Renderer2D * renderer)
 {
-	// draw turret
+	// draw turret with origin at bottom of sprite
 	renderer->drawSpriteTransformed3x3(m_turretSprite, (float*)m_globalTransform, 0.0f, 0.0f, 0.0f, 0.5f, 0.0f);
 	SceneObject::draw(renderer);
 }
@@ -29,6 +28,7 @@ void TankTurret::draw(aie::Renderer2D * renderer)
 void TankTurret::notifyCollision(SceneObject * other, Vector2 penetration)
 {
 	if (other != m_parent && m_parent != nullptr) {
+		// If collision wasn't with parent, treat as parent colliding with other object
 		m_parent->notifyCollision(other, penetration);
 	}
 }
@@ -36,15 +36,18 @@ void TankTurret::notifyCollision(SceneObject * other, Vector2 penetration)
 void TankTurret::notifyOutOfBounds(Vector2 penetration)
 {
 	if (m_parent != nullptr) {
+		// Treat as parent being out of bounds
 		m_parent->notifyOutOfBounds(penetration);
 	}
 }
 
 void TankTurret::setupCollider()
 {
+	// Create OBox if collider doesn't exist
 	if (m_collider == nullptr) {
 		m_collider = new OBox();
 	}
+	// Place and orient OBox around sprite
 	((OBox*)m_collider)->setHalfExtents((Vector2)m_globalTransform[0] * 8, (Vector2)m_globalTransform[1] * 25);
 	((OBox*)m_collider)->setCentre((Vector2)m_globalTransform[2]+ (Vector2)m_globalTransform[1] * 25);
 }
